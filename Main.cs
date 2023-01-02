f222d90b82f198dde6bd8b0b0cac78e426376924
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2023-01-02 17:50:35 -0500

    Adding tax brackets and tax rates

--- Main.cs
+++ Main.cs
@@ -5,13 +5,14 @@ namespace FinancialCalculator {
         }
 
         private void calcButton_Click(object sender, EventArgs e) {
+            double[] fedTaxRates = {15, 20.5, 26, 29, 33};
+            long[] fedTaxBrackets = {50_195, 100_392, 155_625, 221_708, 100_000_000};
             long income;
             String province;
 
             // grabbing variables
             if (long.TryParse(incomeTextBox.Text, out income));
             province = provinceListBox.Text.Trim();
-
 
             // showing take home labels
             takeHomeLabel.Visible = true;
