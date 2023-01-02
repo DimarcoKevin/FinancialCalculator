f5afa08fbbd207fda7d3a68dc36b7b5a4049e2b8
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2023-01-02 17:31:06 -0500

    Adding province box

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -25,6 +25,8 @@
         private void InitializeComponent() {
             this.incomeLabel = new System.Windows.Forms.Label();
             this.incomeTextBox = new System.Windows.Forms.TextBox();
+            this.provinceLabel = new System.Windows.Forms.Label();
+            this.provinceListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // incomeLabel
@@ -44,11 +46,47 @@
             this.incomeTextBox.Size = new System.Drawing.Size(169, 23);
             this.incomeTextBox.TabIndex = 1;
             // 
+            // provinceLabel
+            // 
+            this.provinceLabel.AutoSize = true;
+            this.provinceLabel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.provinceLabel.Location = new System.Drawing.Point(73, 132);
+            this.provinceLabel.Name = "provinceLabel";
+            this.provinceLabel.Size = new System.Drawing.Size(85, 25);
+            this.provinceLabel.TabIndex = 2;
+            this.provinceLabel.Text = "Province";
+            // 
+            // provinceListBox
+            // 
+            this.provinceListBox.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.provinceListBox.FormattingEnabled = true;
+            this.provinceListBox.ItemHeight = 25;
+            this.provinceListBox.Items.AddRange(new object[] {
+            "Alberta",
+            "British Columbia",
+            "Manitoba",
+            "New Brunswick",
+            "Newfoundland and Labrador",
+            "Northwest Territories",
+            "Nova Scotia",
+            "Nunavut",
+            "Ontario",
+            "Prince Edward Island",
+            "Quebec",
+            "Saskatchewan",
+            "Yukon"});
+            this.provinceListBox.Location = new System.Drawing.Point(73, 160);
+            this.provinceListBox.Name = "provinceListBox";
+            this.provinceListBox.Size = new System.Drawing.Size(169, 29);
+            this.provinceListBox.TabIndex = 3;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1187, 634);
+            this.Controls.Add(this.provinceListBox);
+            this.Controls.Add(this.provinceLabel);
             this.Controls.Add(this.incomeTextBox);
             this.Controls.Add(this.incomeLabel);
             this.Name = "Main";
@@ -62,5 +100,7 @@
 
         private Label incomeLabel;
         private TextBox incomeTextBox;
+        private Label provinceLabel;
+        private ListBox provinceListBox;
     }
 }
